34736b9f1fdf35e6f7e4b5b8348ab73741bdf11a
Star-tears <star-tears@outlook.com>
2024-05-23 03:06:37 -0700

    feat: add create_bucket api

--- server/app/api/routes/kv.py
+++ server/app/api/routes/kv.py
@@ -1,7 +1,10 @@
+import os
 from app.api.deps import SessionDep
+from app.common.config import Config
 from app.models.kv_items import BucketItemBase, KvItemBase
 from app.models.response import ResponseBase
 from app.models.sql_models import KvData
+from app.utils.resource import create_folder
 from fastapi import APIRouter, Depends, File, UploadFile
 from fastapi.responses import JSONResponse
 
@@ -20,4 +23,11 @@ def get_kv_data(data: BucketItemBase, session: SessionDep):
 @router.post("/update_kv", response_model=ResponseBase)
 def update_kv(data: KvItemBase, session: SessionDep):
     kv_data = crud.upsert_kv(session, data.key, data.value, data.bucketName)
-    return ResponseBase(code=0, data={})
+    return ResponseBase(code=0, data=kv_data)
+
+
+@router.post("/create_bucket", response_model=ResponseBase)
+def create_bucket(data: BucketItemBase):
+    path = os.path.join(Config.WEBSERVER, "kv", "buckets", data.bucketName)
+    result = create_folder(path)
+    return ResponseBase(code=0, data={"result": result})
--- server/app/utils/resource.py
+++ server/app/utils/resource.py
@@ -14,3 +14,29 @@ def get_folder_list(path):
             folder_list.append(folder)
 
     return folder_list
+
+
+def create_folder(path):
+    """
+    创建文件夹
+    :param path: 文件夹路径
+    :return: 是否创建成功
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+        return True
+    else:
+        return False
+
+
+def delete_folder(path):
+    """
+    删除文件夹
+    :param path: 文件夹路径
+    :return: 是否删除成功
+    """
+    if os.path.exists(path):
+        os.rmdir(path)
+        return True
+    else:
+        return False
