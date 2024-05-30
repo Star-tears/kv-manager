374b52a4400531248a65d5627900311fee294061
Star-tears <star-tears@outlook.com>
2024-05-30 20:19:01 +0800

    feat: use dotenv

--- server/app/main.py
+++ server/app/main.py
@@ -12,6 +12,7 @@ import uvicorn
 from app.api.main import api_router
 from app.core.config import settings
 from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
 import mimetypes
 
 mimetypes.init()
@@ -19,46 +20,41 @@ mimetypes.add_type("application/javascript", ".js")
 mimetypes.add_type("text/css", ".css")
 mimetypes.add_type("image/svg+xml", ".svg")
 
+# 加载.env文件，如果存在的话
+load_dotenv(".env", override=False)  # 设置override=False表示环境变量优先
+active_env = os.getenv("ACTIVE_ENV")
+load_dotenv(f".env.{active_env}", override=False)
+
+address = os.getenv("KV_SERVER_ADDRESS", "0.0.0.0")
+port = int(os.getenv("KV_SERVER_PORT", 11028))
+Config.WEBSERVER = os.getenv("KV_WEBSERVER_PATH", ".web-server")
+Config.FRONTEND = os.getenv("KV_FRONTEND_PATH", "dist")
+
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--address", type=str, default="0.0.0.0", help="server listen address"
-    )
-    parser.add_argument("--port", type=int, default=11028, help="server listen port")
-    parser.add_argument(
-        "--webserver-path", type=str, default=".web-server", help="webserver path"
-    )
-    parser.add_argument(
-        "--frontend-path", type=str, default="dist", help="frontend path"
-    )
-    args = parser.parse_args()
-    Config.FRONTEND = os.path.join(args.frontend_path)
-    Config.WEBSERVER = os.path.join(args.webserver_path)
-    db.update_engine()
-    app = FastAPI(
-        title=settings.PROJECT_NAME,
-        openapi_url=f"{settings.API_V1_STR}/openapi.json",
-        generate_unique_id_function=custom_generate_unique_id,
-    )
-    app.include_router(api_router, prefix=settings.API_V1_STR)
-    app.mount(
-        "/assets",
-        StaticFiles(directory=os.path.join(Config.FRONTEND, "assets")),
-        name="static",
-    )
-    templates = Jinja2Templates(directory=os.path.join(Config.FRONTEND))
+db.update_engine()
 
-    @app.get("/", response_class=HTMLResponse, tags=["index"])
-    async def home(request: Request):
-        return templates.TemplateResponse("index.html", {"request": request})
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+app.include_router(api_router, prefix=settings.API_V1_STR)
+app.mount(
+    "/assets",
+    StaticFiles(directory=os.path.join(Config.FRONTEND, "assets")),
+    name="static",
+)
+templates = Jinja2Templates(directory=os.path.join(Config.FRONTEND))
 
-    uvicorn.run(app, host=args.address, port=args.port, reload=False)
+
+@app.get("/", response_class=HTMLResponse, tags=["index"])
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host=address, port=port, reload=False)
